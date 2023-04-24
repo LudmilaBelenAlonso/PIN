e98f86fbcd84d1a6554bd299c6ac9614e9e81158
ludmilabelenalonso <ludmila.b.alonso@gmail.com>
2023-04-24 10:46:19 -0300

    manifiesto terraform
    levanta un contenedor jenkins con el docker.sock, expuesto en puerto 8080

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+resource "docker_network" "jenkins" {
+  name = "jenkins"
+}
+resource "docker_volume" "jenkins_home" {
+  name = "jenkins_home"
+}
+
+resource "docker_container" "jenkins" {
+  image = "jenkins/jenkins"
+  name = "jenkins"
+  ports {
+    internal = 8080
+    external = 8080
+  }
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+  network_mode = "bridge"
+  networks_advanced {
+  name = docker_network.jenkins.name
+  }
+}
